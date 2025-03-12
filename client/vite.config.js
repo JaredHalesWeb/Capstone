c1c72ef988a854000f4a2875893b312bf55e355f
jared hales <jared.hales2838@STU.MTEC.EDU>
2025-03-12 09:35:31 -0600

    render and mongo and lint is here

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,9 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port:3001,
+    port:3000,
     proxy: {
-      "/api": "http://localhost:3001",
+      "/api": "http://localhost:3000",
     },
   },
 });
--- server/server.js
+++ server/server.js
@@ -1,8 +1,20 @@
 // server/server.js
 const path = require("path");
 const express = require("express");
+const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3001;
+const databaseUrl = process.env.DATABASE_URL;
+
+mongoose
+  .connect(databaseUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
